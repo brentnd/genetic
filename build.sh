e03c213f36727c89d725ccea290f76d1a3c6c5b9
Brent Dimmig <brentdimmig@gmail.com>
2015-07-12 12:58:28 -0400

    add command line args for evolution parameters

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
    cd target
    cmake ../
    make -j4
+   ./genetic -e -m 0.1 -p 30 -g 10
    ./test_genetic
 )
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,60 @@
 #include <stdio.h>
 #include <cstdlib>
+#include <getopt.h>
 
 #include "population.hpp"
 #include "individual.hpp"
+#include "random.hpp"
 
-int main() {
+int main( int argc, char * const * argv ) {
+   // Parameter defaults
+   bool elitism = false;
+   int max_generations = 50;
+   int population_size = 99;
+   double mutation_rate = 0.1;
+
+   opterr = 0;
+   char c;
+   while ((c = getopt(argc, argv, "eg:m:p:")) != -1) {
+      switch (c) {
+      case 'e':
+         elitism = true;
+         break;
+      case 'm':
+         mutation_rate = atof(optarg);
+         break;
+      case 'g':
+         max_generations = atoi(optarg);
+         break;
+      case 'p':
+         population_size = atoi(optarg);
+         break;
+      case '?':
+         if (optopt == 'g' || optopt == 'p')
+            fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+         else if (isprint (optopt))
+            fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+         else
+            fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+         break;
+      }
+   }
 
    int max = genetic::individual::get_max_fitness();
-   genetic::population pop(999);
-   int gen = 0;
+   random::set_seed(1);
+   genetic::population pop(population_size);
+
+   int generation = 0;
    int best;
    do {
       genetic::individual fittest = pop.get_fittest();
       best = fittest.get_fitness();
-      printf("Generation %d: Fittness: %d ", gen, best);
+      printf("Generation %d: Fittness: %d ", generation, best);
       fittest.print_genes();
-      if (gen++ > 500) {
+      if (generation++ > max_generations) {
          break;
       }
-      pop.evolve(0.1, true);
+      pop.evolve(mutation_rate, elitism);
    } while (best < max);
    return 0;
 }
